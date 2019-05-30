6b964743c9f038cf9c2f593578d251ffa7c08ee2
guava <399852658@>
2019-05-30 21:32:03 +0800

    index title both

--- serp/baidu_index.py
+++ serp/baidu_index.py
@@ -25,15 +25,16 @@ def get_index_baidu(site):
 		print(site+' 收录数：0')
 
 def get_title(site):
-	print('http://www.'+site)
+	# print('http://www.'+site)
 	html = get('http://www.'+site)
 	#print(html)
 	soup = BeautifulSoup(html, 'html.parser')
 	print(soup.title)
 
 for item in list1:
-	#get_index_baidu(item)
+
 	try:
+		get_index_baidu(item)
 		get_title(item)
 	except IOError:
 		print("Error: 没有找到文件或读取文件失败")
