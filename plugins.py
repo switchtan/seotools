51c6efe347e72f36c8289a8f1dc0cfe8bc0f04e8
switchtan <399852658@qq.com>
2019-08-23 12:41:08 +0800

    Update plugins.py

--- plugins.py
+++ plugins.py
@@ -3,7 +3,7 @@
 import os
 #delete-duplicate-posts
 #list1 = ['disable-google-fonts','autoptimize','all-in-one-seo-pack', 'wp-super-cache','spiderdisplay','autoptimize','wp-baidu-submit','wptouch','baidu-submit-link','i3geek-mip','i3geek-baiduxzh','broken-link-checker']
-list1=['wp-fastest-cache','disable-google-fonts','autoptimize','advanced-excerpt']
+list1=['wp-fastest-cache','disable-google-fonts','autoptimize','advanced-excerpt','wp-baidu-submit']
 str=""
 for item in list1:
 	str=str+"wget --quiet http://downloads.wordpress.org/plugin/"+item+".zip\n"
